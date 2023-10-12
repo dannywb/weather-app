04abf1ee68d65726bbf8735e64d4d6048fe8a1dd
Danny Brown <dannywb@gmail.com>
2023-10-12 10:30:28 -0400

    Updated name AGAIN

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('', (req, res) => {
   res.render('index', {
-    title: 'Weather',
+    title: 'Weather App',
     name: 'Danny Brown'
   })
 })
